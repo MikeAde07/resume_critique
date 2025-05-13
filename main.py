3b7f8023be2597effc540a89ce389b9ddbdc7e98
Michael Adeniran <MikeAde07@gmail.com>
2025-05-12 19:16:47 -0500

    updates

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import PyPDF2
+import io
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.set_page_config(page_title="AI Resume Critiquer", page_icon="📃", layout="centered")
+
+st.title("AI Resume Critiquer")
+st.markdown("Upload your resume and get AI-powered feedback tailored to your needs!")
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+uploaded_file = st.file_uploader("Upload your resume (PDF or TXT)", type=["pdf", "txt"])
+job_role = st.text_input("Enter the job role you're targetting (optional)")
+
+analyze = st.button("Analyze Resume")
+
+def extract_text_from_pdf(pdf_file) :
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    text = "" 
+    for page in pdf_reader.pages :
+        text += page.extract_text() + "\n"
+    return text
+
+def extract_text_from_file(uploaded_file) :
+    if uploaded_file.type == "application/pdf" :
+        return extract_text_from_pdf(io.BytesIO(uploaded_file.read()))
+
+
+if analyze and uploaded_file :
+    try :
+        file_content = 
